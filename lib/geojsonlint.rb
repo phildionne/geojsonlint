99371d80be773e4a881704967d66afb487357877
Philippe Dionne <dionne.phil@gmail.com>
2015-05-01 18:09:34 -0400

    Add geojsonlint core

--- lib/geojsonlint.rb
+++ lib/geojsonlint.rb
@@ -1,5 +1,60 @@
-require "geojsonlint/version"
+require 'geojsonlint/version'
+require 'json'
+require 'json-schema'
 
 module Geojsonlint
-  # Your code goes here...
+
+  class Parser
+    attr_reader :schema, :data
+
+    # @param data [String, Hash, File]
+    def initialize(data)
+      @data = data
+      @schema = JSON.parse(File.read('./lib/geojsonlint/geojson_schema.json'))
+      self
+    end
+
+    # @return [Geojson]
+    def run
+      options = {
+        errors_as_objects: true,
+        validate_schema: true
+      }
+
+      geojson = Geojson.new
+      geojson.errors = JSON::Validator.fully_validate(@schema, @data, **options)
+
+      geojson
+    end
+  end
+
+  class Geojson
+    attr_accessor :errors
+
+    # @return [Geojson]
+    def initialize
+      self.errors = Array.new
+      self
+    end
+
+    # @return [Boolean]
+    def valid?
+      !invalid?
+    end
+
+    # @return [Boolean]
+    def invalid?
+      errors.any?
+    end
+  end
+
+  # Validates a geojson object
+  # @param geojson [String, Hash, File] a JSON string, a file containing JSON,
+  # or a Ruby object representing JSON data
+  #
+  # @return [Geojson]
+  def validate(geojson)
+    Parser.new(geojson).run
+  end
+  module_function :validate
 end
--- spec/geojsonlint_spec.rb
+++ spec/geojsonlint_spec.rb
@@ -1,11 +1,201 @@
 require 'spec_helper'
 
 describe Geojsonlint do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Geojsonlint::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe :ModuleMethods do
+    let(:support_path) { File.expand_path('../support', __FILE__) }
+
+    describe :validate do
+
+      context "with valid" do
+
+        describe "json" do
+          let(:json) { JSON.dump({ foo: "bar" }) }
+
+          it "does not raise error" do
+            expect {
+              Geojsonlint.validate(json)
+            }.not_to raise_error
+          end
+
+          it "populates errors" do
+            geojson = Geojsonlint.validate(json)
+            expect(geojson.errors).not_to be_empty
+          end
+        end
+
+        describe "geometry" do
+
+          describe "Point" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'point.json'))) }
+
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+
+          describe "multiPoint" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'multipoint.json'))) }
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+
+          describe "lineString" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'linestring.json'))) }
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+
+          describe "multiLineString" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'multilinestring.json'))) }
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+
+          describe "Polygon" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'polygon.json'))) }
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+
+          describe "MultiPolygon" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'multipolygon.json'))) }
+
+            it "is valid" do
+              expect(Geojsonlint.validate(geojson)).to be_valid
+            end
+          end
+        end
+
+        describe "geometry colletion" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'geometrycollection.json'))) }
+
+          it "is valid" do
+            expect(Geojsonlint.validate(geojson)).to be_valid
+          end
+        end
+
+        describe "feature" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'feature.json'))) }
+
+          it "is valid" do
+            expect(Geojsonlint.validate(geojson)).to be_valid
+          end
+        end
+
+        describe "feature collection" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'featurecollection.json'))) }
+
+          it "is valid" do
+            expect(Geojsonlint.validate(geojson)).to be_valid
+          end
+        end
+      end
+
+      context "with invalid" do
+
+        describe "json" do
+          let(:json) { '{foo:"bar"}' }
+
+          it "does not raise error" do
+            expect {
+              Geojsonlint.validate(json)
+            }.not_to raise_error
+          end
+
+          it "populates errors" do
+            geojson = Geojsonlint.validate(json)
+            expect(geojson.errors).not_to be_empty
+          end
+        end
+
+        describe "geometry" do
+
+          describe "Point" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_point.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+
+          describe "multiPoint" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_multipoint.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+
+          describe "lineString" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_linestring.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+
+          describe "multiLineString" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_multilinestring.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+
+          describe "Polygon" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_polygon.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+
+          describe "MultiPolygon" do
+            let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_multipolygon.json'))) }
+
+            it "is invalid" do
+              expect(Geojsonlint.validate(geojson)).to be_invalid
+            end
+          end
+        end
+
+        describe "geometry colletion" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_geometrycollection.json'))) }
+
+          it "is invalid" do
+            expect(Geojsonlint.validate(geojson)).to be_invalid
+          end
+        end
+
+        describe "feature" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_feature.json'))) }
+
+          it "is invalid" do
+            expect(Geojsonlint.validate(geojson)).to be_invalid
+          end
+        end
+
+        describe "feature collection" do
+          let(:geojson) { JSON.parse(File.read(File.join(support_path, 'invalid_featurecollection.json'))) }
+
+          it "is invalid" do
+            expect(Geojsonlint.validate(geojson)).to be_invalid
+          end
+        end
+      end
+    end
   end
 end
