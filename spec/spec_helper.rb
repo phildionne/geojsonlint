ef5ea93433a4bf56e16a646d5bb5388a2d94e059
Philippe Dionne <dionne.phil@gmail.com>
2015-05-01 18:07:13 -0400

    Configure spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'geojsonlint'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].map(&method(:require))
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.filter_run_excluding skip: true
+  config.run_all_when_everything_filtered = true
+end
