af78220e5ebcd7bd8c65a0dca27bf6ac31d11927
Philippe Dionne <dionne.phil@gmail.com>
2015-05-09 15:23:08 -0400

    Fix validator message

--- lib/geojsonlint/geojson_validator.rb
+++ lib/geojsonlint/geojson_validator.rb
@@ -4,7 +4,7 @@ class GeojsonValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     unless valid_geojson?(value)
-      message = options[:message] || "Invalid geojson"
+      message = options[:message] || "invalid"
       record.errors.add(attribute, message)
     end
   end
